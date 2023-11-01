b0867fc47753baf29bd2e98047c2fd8530bf4285
Cristian Besada <114959857+cristianbesada-laika@users.noreply.github.com>
2023-11-01 20:41:52 -0300

    Add inputs to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import './App.css';
 import Card from "./components/Card";
+import { useState } from 'react';
 
 function App() {
+  const [usuario, setUsuario]= useState({
+    nombre: "",
+    autor: "",
+  })
+  const [enviado, setEnviado] = useState(false)
+  const [error, setError] = useState(false)
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (usuario.nombre.trim().length > 2 && usuario.autor.length > 5) {
+      setEnviado(true)
+      setError(false)
+    } else {
+      setError(true)
+      setEnviado(false)
+    }
+  }
+
   return (
+    <>
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Autores</h1>
+      <form onSubmit={onSubmitForm} style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+        <label>Ingresa tu nombre</label>
+        <input type="text" onChange={(event) => setUsuario({...usuario, nombre: event.target.value})}/>
+
+        <label>Ingresa tu autor favorito</label>
+        <input type="text" onChange={(event) => setUsuario({...usuario, autor: event.target.value})}/>
+
+        <button type='submit'>Enviar</button>
+          {enviado && <Card usuario={usuario}/>}
+          {error && <h3 style={{color: 'red'}}>“Por favor chequea que la información sea correcta”.</h3>}
+      </form>
     </div>
+    </>
   );
 }
 
